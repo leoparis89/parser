53e8725f3121a14eaae08b16c28bd5e7d4e79cfa
Lev Kowalski <levkowalski@gmail.com>
2022-04-14 16:18:42 +0200

    wip

--- src/Parser.js
+++ src/Parser.js
@@ -1,6 +1,15 @@
+const { Tokenizer } = require("./Tokenizer");
 class Parser {
+  constructor() {
+    this._string = "";
+    this._tokenizer = new Tokenizer();
+  }
+
   parse(string) {
     this._string = string;
+    this._tokenizer.init(string);
+
+    this._lookahead = this._tokenizer.getNextToken();
     return this.Program();
   }
 
@@ -12,10 +21,25 @@ class Parser {
   }
 
   NumericLiteral() {
+    const token = this._eat("NUMBER");
     return {
       type: "NumericLiteral",
-      value: Number(this._string),
+      value: Number(token.value),
     };
+  }
+
+  _eat(tokenType) {
+    const token = this._lookahead;
+    if (token == null) {
+      throw new SyntaxError(`Unexpected end of input, expected ${tokenType}`);
+    }
+
+    if (token.type != tokenType) {
+      throw new SyntaxError(`Unexpected tokenr, expected ${tokenType}`);
+    }
+
+    this._lookahead = this._tokenizer.getNextToken();
+    return token;
   }
 }
 
