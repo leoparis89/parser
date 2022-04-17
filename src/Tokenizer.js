19ca7d034dbe3feafd5db4ca149cb13d015e91ec
Lev Kowalski <levkowalski@gmail.com>
2022-04-17 16:12:23 +0200

    îp

---
+++ src/Tokenizer.js
@@ -0,0 +1,55 @@
+const Specs = [
+  [/^\d+/, "NUMBER"],
+  [/"[^"]*"/, "STRING"],
+  [/'[^']*'/, "STRING"],
+];
+
+class Tokenizer {
+  init(string) {
+    this._string = string;
+    this._cursor = 0;
+  }
+
+  isEOF() {
+    return this._cursor === this._string.length;
+  }
+
+  hasMoreTokens() {
+    return this._cursor < this._string.length;
+  }
+
+  getNextToken() {
+    if (!this.hasMoreTokens()) {
+      return null;
+    }
+
+    const string = this._string.slice(this._cursor);
+
+    for (const [regexp, tokenType] of Specs) {
+      let tokenValue = this._match(regexp, string);
+
+      if (tokenValue == null) {
+        continue;
+      }
+
+      return {
+        type: tokenType,
+        value: tokenValue,
+      };
+    }
+
+    throw new TypeError(`Unexpected token: "${string[0]}"`);
+  }
+
+  _match(regexp, string) {
+    let matched = regexp.exec(string);
+    if (matched == null) {
+      return null;
+    }
+
+    this._cursor += matched[0].length;
+    return matched[0];
+  }
+}
+
+module.exports = { Tokenizer };
