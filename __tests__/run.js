2617603ef350a96ffa9e2258e374b828ea203937
Lev Kowalski <levkowalski@gmail.com>
2022-04-17 15:40:37 +0200

    wip

--- __tests__/run.js
+++ __tests__/run.js
@@ -1,6 +1,6 @@
 const { Parser } = require("../src/Parser");
 const parser = new Parser();
-const program = `'foof'`;
+const program = `"foo"`;
 
 const ast = parser.parse(program);
 console.log(JSON.stringify(ast, null, 2));
