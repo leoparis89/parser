a504d77dd9e2688f405d48e7f0ef30235a2f75ec
Lev Kowalski <levkowalski@gmail.com>
2022-04-19 21:25:36 +0200

    tests

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -1,19 +1,38 @@
 const { Parser } = require("../src/Parser");
 
 describe("parser", () => {
-  test("parses numbers and ignores comments", () => {
+  test("numbers", () => {
     const parser = new Parser();
-    const program = `/*
-   hello
-*/
-
-32
-`;
+    const program = `32`;
 
     const ast = parser.parse(program);
 
     expect(ast).toEqual({
       body: { type: "NumericLiteral", value: 32 },
+      type: "Program",
+    });
+  });
+
+  test("strings double quote", () => {
+    const parser = new Parser();
+    const program = `"hello"`;
+
+    const ast = parser.parse(program);
+
+    expect(ast).toEqual({
+      body: { type: "StringLiteral", value: "hello" },
+      type: "Program",
+    });
+  });
+
+  test("strings single quote", () => {
+    const parser = new Parser();
+    const program = `'hello'`;
+
+    const ast = parser.parse(program);
+
+    expect(ast).toEqual({
+      body: { type: "StringLiteral", value: "hello" },
       type: "Program",
     });
   });
